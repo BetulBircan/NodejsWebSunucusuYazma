c7539a0ef4447d5a96941759e5ce46de81b91861
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-03-01 16:01:03 +0300

    web sunucusu yazmak için http modülü tanımlandı

--- server.js
+++ server.js
@@ -7,3 +7,7 @@ index, hakkimda ve iletisim sayfaları oluşturalım.
 Sayfalara içerik olarak xxx sayfasına hoşgeldiniz şeklinde h2 başlıkları yazdıralım.
 port numarası olarak 5000'i kullanalım.
  */
+
+//node.js tarafında bir web sunucusu yazmak için node.js core modülü olan http modülünden faydalanacağız. 
+//http tanımlama
+const http = require('http')
